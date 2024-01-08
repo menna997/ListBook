6abf722b33117363f22310ff24788b696f4ab321
Mennatallah Helmy <Mennatallah.Helmy@ad.cyshield>
2024-01-08 10:08:56 +0200

    edit button add book

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const NameBook = document.querySelector("#title");
-
-const Author = document.querySelector("#Author");
-const Year = document.querySelector("#Year");
+var NameBook= document.getElementById('nameBook');
+var Author = document.getElementById('author');
+var Year = document.getElementById('year');
 const submit = document.querySelector(".btn");
-const list = document.querySelector("#list-book");
+const list = document.querySelector("#listBook");   
 
 
 submit.addEventListener('click', function (e) {
     e.preventDefault();
     if (NameBook.value == '' && Author.value == '' && Year.value == '') {
-        JSON.parse(localStorage.getItem('user'));
-        const NameBook= JSON.parse(localStorage.getItem('title'));
+
         console.log(NameBook);
-        // alert("please fill all input");
+   
         NameBook.classList.add('warning');
         Author.classList.add('warning');
         Year.classList.add('warning');
@@ -23,7 +21,7 @@ submit.addEventListener('click', function (e) {
             NameBook.classList.remove('warning');
             Author.classList.remove('warning');
             Year.classList.remove('warning');
-
+    
             NameBook.setAttribute("placeholder", "");
             Author.setAttribute("placeholder", "");
             Year.setAttribute("placeholder", "");
@@ -43,7 +41,7 @@ submit.addEventListener('click', function (e) {
         Author.setAttribute('placeholder', 'write name Author');
         function removeAuthorWarning() {
             Author.classList.remove('warning');
-          Author.setAttribute("placeholder", "");
+            Author.setAttribute("placeholder", "");
         }
         setTimeout(removeAuthorWarning, 1000)
     } else if (Year.value == '') {
@@ -54,49 +52,124 @@ submit.addEventListener('click', function (e) {
             Year.setAttribute("placeholder", "");
         }
         setTimeout(removeYearWarning, 1000)
-    } else {
+    }
+
+
+
+    else {
+
         const newRow = document.createElement('tr');
-        list.appendChild(newRow);
-
+    
         const newNameBookColum = document.createElement('th');
-        list.appendChild(newNameBookColum);
-        newNameBookColum.innerText = NameBook.value; 
+     
+        newNameBookColum .innerText = NameBook.value;
+        newRow.appendChild(newNameBookColum);
+     let NameBooks= JSON.parse(localStorage.getItem('NameBooks'))
+        if (!NameBooks) {
+            NameBooks = [];
+        }
+     
+        NameBook.push(JSON.stringify(NameBook.value))
+     
+        localStorage.setItem('NameBook', JSON.stringify(NameBook));
 
-        
-            localStorage.setItem('NameBook',JSON.stringify(NameBook.value));
-        
-        const storedBlogs = JSON.parse(localStorage.getItem('NameBook'));
-        console.log(s)
+    
         const newAuthorColum = document.createElement('th');
     
-        list.appendChild(newAuthorColum);
         newAuthorColum.innerText = Author.value;
-        
-        localStorage.setItem('Author', Author.value);
+        newRow.appendChild(newAuthorColum);
+        if (!authors) {
+            authors = [];
+        }
+   
+        authors.push(JSON.stringify(Author.value))
+     
+        localStorage.setItem('Author', JSON.stringify(authors));
         const newYearColum = document.createElement('th');
         
-        list.appendChild(newYearColum);
         newYearColum.innerText = Year.value;
+        newRow.appendChild(newYearColum);
         
-        localStorage.setItem('Year', Year.value);
+        if (!years) {
+            years = [];
+        }
 
-        const newIconColum = document.createElement('th')
-        list.appendChild(newIconColum)
-        newIconColum.innerHTML =  `
-      <a href="#" class="btn btn-danger btn-sm delete">X</a>
-      `
-        // <th><i class="bi bi-trash3-fill"></i></th>z
+        years.push(JSON.stringify(Year.value))
+        localStorage.setItem('Year', JSON.stringify(years));
+
+        const newIconColum = document.createElement('td')
+        newIconColum.innerHTML = `
+        <button type="button" class="btn btn-outline-danger"> Delete</button>
+        `
+        newRow.appendChild(newIconColum)
         
     }
+
     NameBook.value = '';
     Author.value = '';
     Year.value = '';
-})
-function add(){
- list.push(NameBook.value);
-    localStorage.getItem('title', JSON.stringify(list));
-    console.log(list)
-    addTask(NameBook.value);
-    NameBook.value = '';
-  }
-  
+});
+
+
+
+function getLocalStorageData(arr) {
+    let localStorageNameBook = JSON.parse(localStorage.getItem('NameBook'));
+    
+    let localStorageAuthor = JSON.parse(localStorage.getItem('Author'));
+    let localStorageYear = JSON.parse(localStorage.getItem('Year'));
+    let tableRow = document.createElement('tr');
+    let tableRowAuthor = document.createElement('tr');
+    let tableRowYear = document.createElement('tr');
+
+    console.log(tableRow)
+
+    localStorageNameBook.forEach(SingleNameBook => {
+        let tableData = document.createElement('td');
+
+        tableData.innerHTML = SingleNameBook.replace('"', '').replace('"', '');
+        tableRow.appendChild(tableData);
+   
+    });
+    localStorageAuthor.forEach(SingleAuthor => {
+        let tableDataAuthor = document.createElement('td');
+        tableDataAuthor.innerHTML = SingleAuthor.replace('"', '').replace('"', '');
+        tableRowAuthor.appendChild(tableDataAuthor);
+    });   localStorageYear.forEach(SingleYear => {
+        let tableDataYear = document.createElement('td');
+    
+        tableDataYear.innerHTML = SingleYear.replace('"', '').replace('"', '');
+        tableRowYear.appendChild(tableDataYear);
+    });
+    list.appendChild(tableRow);
+    list.appendChild(tableRowAuthor);
+    list.appendChild(tableRowYear)
+}
+
+
+
+
+//   var x = document.forms["form-book"]["nameBook"].value;
+//     var a = document.forms["form-book"]["author"].value;
+//     var y = document.forms["form-book"]["year"].value;
+//         if (x == "" && a=="" && y=="") {
+//             alert("input must be filled out");
+//           return false;
+//     }
+//     if (x == "" ) {
+//         // alert("Name must be filled out");
+    
+//         x.classList.add('warning');
+//       return false;
+// }
+//     if (a == "") {
+//         alert("Author must be filled out");
+        
+        
+         
+//         return false;
+//     }    if ( y=="") {
+//         alert("Year must be filled out");
+    
+     
+//       return false;
+// }
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var NameBookInput = document.getElementById('nameBook');
 var AuthorInput = document.getElementById('author');
 var YearInput = document.getElementById('year');
+
 console.log(NameBookInput)
 
 
@@ -9,19 +10,90 @@ if (localStorage.getItem('Books') != null) {
     productContainer = JSON.parse(localStorage.getItem('Books'))
     displayBook(productContainer)
 }
-function addName() {
+ function addName(e) {
+
     var product = {
         Name: NameBookInput.value,
         Author: AuthorInput.value,
         Year:YearInput.value
     }
+
+        if (NameBookInput.value == '' && AuthorInput.value == '' && YearInput.value == '') {
+
+            console.log(NameBookInput);
+   
+            NameBookInput.classList.add('warning');
+       
+            AuthorInput.classList.add('warning');
+
+            YearInput.classList.add('warning');
+     
+            NameBookInput.setAttribute('placeholder', 'write name book');
+      
+            AuthorInput.setAttribute('placeholder', 'write name Author');
+        
+            YearInput.setAttribute('placeholder', 'write name Year');
+            
+            e.preventDefault();
+            removeWarning()
+        }
+        if (NameBookInput.value == '') {
+            NameBookInput.classList.add('warning');
+            
+            // alert("Year must be filled out");
+            NameBookInput.setAttribute('placeholder', 'write name book');
+            e.preventDefault();
+            removeNameWarning()
+        
+        } if (AuthorInput.value == '') {
+            AuthorInput.classList.add('warning');
+            AuthorInput.setAttribute('placeholder', 'write name Author');
+            e.preventDefault();
+            removeAuthorWarning()
+        } if (YearInput.value == '') {
+            YearInput.classList.add('warning');
+            YearInput.setAttribute('placeholder', 'write name Year');
+            e.preventDefault();
+            removeYearWarning()
+       
+        }
+
+
     console.log(product)
     productContainer.push(product);
     localStorage.setItem("Books", JSON.stringify(productContainer));
     console.log(productContainer);
  displayBook(productContainer)
-    clearForm();
+       clearForm();
+    
+ }
+function removeWarning() {
+    NameBookInput.classList.remove('warning');
+    AuthorInput.classList.remove('warning');
+    YearInput.classList.remove('warning');
+
+    NameBookInput.setAttribute("placeholder", "");
+    AuthorInput.setAttribute("placeholder", "");
+    YearInput.setAttribute("placeholder", "");
+
 }
+setTimeout(removeWarning, 9000)
+function removeNameWarning() {
+    NameBookInput.classList.remove('warning');
+    NameBookInput.setAttribute("placeholder", "");
+}
+setTimeout(removeWarning, 9000)
+function removeAuthorWarning() {
+    AuthorInput.classList.remove('warning');
+    AuthorInput.setAttribute("placeholder", "");
+}
+setTimeout(removeAuthorWarning, 9000)
+function removeYearWarning() {
+    YearInput.classList.remove('warning');
+    YearInput.setAttribute("placeholder", "");
+    clearForm()
+}
+setTimeout(removeYearWarning, 9000)
 function clearForm() {
     NameBookInput.value = "";
     AuthorInput.value = "";
